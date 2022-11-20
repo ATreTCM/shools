aebf650ccfc466c26e82a83eee130107de37dd8c
Alexey Tretyakov <48726736+ATreTCM@users.noreply.github.com>
2022-11-20 12:22:17 +0200

    Update schema.py

--- schools/schema.py
+++ schools/schema.py
@@ -1,34 +1,39 @@
-
 import graphene
 from graphene import relay
-from graphene_sqlalchemy import SQLAlchemyConnectionField, SQLAlchemyObjectType
+from graphene_sqlalchemy import SQLAlchemyObjectType
 from .models import Hall as HallModel, Lesson as LessonModel, User as UserModel
-
 
 class UserSchema(SQLAlchemyObjectType):
     class Meta:
         model = UserModel
-        interfaces = (relay.Node,)
+     
         
 class HallSchema(SQLAlchemyObjectType):
     class Meta:
         model = HallModel
-        interfaces = (relay.Node,)
+  
         
 class LessonSchema(SQLAlchemyObjectType):
     class Meta:
         model = LessonModel
-        interfaces = (relay.Node,)
+     
         
 class Query(graphene.ObjectType):
-    node = relay.Node.Field()
-    
-    allUsers = SQLAlchemyConnectionField(UserSchema.connection)
-    
-    allHalls = SQLAlchemyConnectionField(HallSchema.connection)
-    
-    allLessons = SQLAlchemyConnectionField(LessonSchema.connection)
-       
 
+    getAllUsers = graphene.List(UserSchema)
+    getAllHalls = graphene.List(HallSchema)
+    getAllLessons = graphene.List(LessonSchema)
+
+    async def resolve_getAllUsers(self, info):
+        query = UserSchema.get_query(info) 
+        return query.all()
+    
+    async def resolve_getAllHalls(root,info):
+        query = HallSchema.get_query(info) 
+        return query.all()
+
+    async def resolve_getAllLessons(root,info):
+        query = LessonSchema.get_query(info) 
+        return query.all()
     
 schema = graphene.Schema(query=Query)
